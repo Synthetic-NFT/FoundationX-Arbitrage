5da6e878872d7d7c2955530d3025d9f25b076d66
mliu <liumengxiong1218@gmail.com>
2022-06-07 23:20:48 -0700

    add interfaces

---
+++ contracts/interfaces/IPool.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: AGPL-3.0
+pragma solidity ^0.8.0;
+
+/**
+ * @title IPool
+ * @author Aave
+ * @notice Defines the basic interface for an Aave Pool.
+ **/
+interface IPool {
+}
