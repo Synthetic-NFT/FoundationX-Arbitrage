ab25ef62ec701d82a654e004612c4b0dcea6b1f6
mliu <liumengxiong1218@gmail.com>
2022-06-13 16:08:28 -0700

    uni v2 pair and factory

--- test/Router.ts
+++ test/Router.ts
@@ -1,13 +1,22 @@
 import { ethers, network } from "hardhat";
-import { IERC20, IUniswapV2Router02, IWETH } from "../typechain";
+import {
+  IERC20,
+  IUniswapV2Factory,
+  IUniswapV2Pair,
+  IUniswapV2Router02,
+  IWETH,
+} from "../typechain";
 import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
 import { Provider } from "@ethersproject/providers";
 
 describe("#Router", function () {
   const ROUTER_ADDRESS = "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D";
+  const FACTORY_ADDRESS = "0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f";
   const WETH_ADDRESS = "0xc778417E063141139Fce010982780140Aa0cD5Ab";
   const USDC_ADDRESS = "0xeb8f08a975Ab53E34D8a0330E0D34de942C95926";
   let router: IUniswapV2Router02;
+  let factory: IUniswapV2Factory;
+  let pair: IUniswapV2Pair;
   let weth: IWETH;
   let usdc: IERC20;
   let owner: SignerWithAddress;
@@ -20,6 +29,11 @@ describe("#Router", function () {
       require("../abi/contracts/UniswapV2Router02.json"),
       provider
     ) as IUniswapV2Router02;
+    factory = new ethers.Contract(
+      FACTORY_ADDRESS,
+      require("../abi/contracts/UniswapV2Factory.json"),
+      provider
+    ) as IUniswapV2Factory;
     weth = new ethers.Contract(
       WETH_ADDRESS,
       require("../abi/contracts/WETH9.json"),
@@ -30,10 +44,24 @@ describe("#Router", function () {
       require("../abi/contracts/USDC.json"),
       provider
     ) as IERC20;
+    pair = new ethers.Contract(
+      await factory.getPair(WETH_ADDRESS, USDC_ADDRESS),
+      require("../abi/contracts/UniswapV2Pair.json"),
+      provider
+    ) as IUniswapV2Pair;
     [owner] = await ethers.getSigners();
   });
 
   it("Swap WETH for USDC", async function () {
+    const [token0Reserve, token1Reserve, _] = await pair.getReserves();
+    const [wethReserve, usdcReserve] =
+      WETH_ADDRESS < USDC_ADDRESS
+        ? [token0Reserve, token1Reserve]
+        : [token1Reserve, token0Reserve];
+
+    console.log("WETH reserve: ", ethers.utils.formatEther(wethReserve));
+    console.log("USDC reserve: ", ethers.utils.formatEther(usdcReserve));
+
     await network.provider.send("hardhat_setBalance", [
       owner.address,
       ethers.utils.parseEther("400").toHexString(),
